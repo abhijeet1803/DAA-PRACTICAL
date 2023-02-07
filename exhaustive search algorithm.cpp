608af8efd5ba57473245cea494b4a49e63e75c90
abhijeet1803 <123958051+abhijeet1803@users.noreply.github.com>
2023-02-07 15:07:10 +0530

    Add files via upload

---
+++ exhaustive search algorithm.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxPackedSets(vector<int>& items,
+				vector<set<int> >& sets)
+{
+
+int maxSets = 0;
+
+// Loop through all the sets
+for (auto set : sets) {
+	int numSets = 0;
+
+	
+	for (auto item : items) {
+	if (set.count(item)
+		numSets += 1;
+
+		
+		items.erase(remove(items.begin(),
+						items.end(), item),
+					items.end());
+	}
+	}
+
+	
+	maxSets = max(maxSets, numSets+1);
+}
+
+return maxSets;
+}
+
+int main()
+{
+
+// Set of items
+vector<int> items = { 1, 2, 3, 4, 5, 6 };
+
+// List of sets
+vector<set<int> > sets
+	= { { 1, 2, 3 }, { 4, 5 }, { 5, 6 }, { 1, 4 } };
+
+
+
+int maxSets
+	= maxPackedSets(items, sets);
+
+// Print the result
+cout << "Maximum number of sets that can be packed: "
+	<< maxSets << endl;
+
+return 0;
+}
+
+
